c445a245d26e32aae316bb67dbd22b85dc70b55a
Evgenatrix <postmanbox@gmail.com>
2023-05-01 16:49:18 +0300

    Update rhvoice.sh

--- rhvoice.sh
+++ rhvoice.sh
@@ -1,6 +1,11 @@
 #! /bin/bash
 # https://launchpad.net/~linvinus/+archive/ubuntu/rhvoice - репозиторий RHVoice для 32 бит
 # http://mxl.mirror.root.lu/mx/repo/pool/main/f/foliate/ - пакеты Foliate от MX Linux и Antix
+# https://rhvoice.su/voices/ - Голоса для RHVoice
+# Информация по конфигам RHVoice для Linux, Windows, Android
+№ /usr/share/RHVoice/voices - Папка с языками
+# /etc/RHVoice/dicts/Russian/ - Папка для пользовательских словарей
+#
 # RHVoice-test -p aleksandr  ***Достаточно этой строки в настройках Foliate.
 # anna, aleksandr, sit
 if [[ $FOLIATE_TTS_LANG_LOWER == 'ru' ]];
